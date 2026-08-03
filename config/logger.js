c2f874df6262cbad537d7a677ee8c5cf1636d09b
Andre Monteiro <dev@andrelam.com.br>
2026-08-02 22:17:18 -0300

    Update Winston to 3.19.0

--- config/logger.js
+++ config/logger.js
@@ -1,17 +1,18 @@
-﻿var config = require('./setup');
-
-const winston = require("winston");
+var config = require('./setup');
+var winston = require('winston');
 
 var level = process.env.LOG_LEVEL || config.logging.level || 'debug';
 
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.Console({
-            level: level,
-            timestamp: function () {
-                return (new Date()).toISOString();
-            }
+var logger = winston.createLogger({
+    level: level,
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(function(info) {
+            return info.timestamp + ' - ' + info.level + ': ' + info.message;
         })
+    ),
+    transports: [
+        new winston.transports.Console()
     ]
 });
 
