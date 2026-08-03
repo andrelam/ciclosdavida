f91a7382cb1ec6d0d1d591c3200f36c7e5111d0b
Andre Monteiro <dev@andrelam.com.br>
2026-08-02 22:34:55 -0300

    Update Express middleware dependencies

--- config/express.js
+++ config/express.js
@@ -5,7 +5,6 @@ var MongoStore = require('connect-mongo').MongoStore;
 var csrf = require('csurf');
 var compression = require('compression');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var morgan = require('morgan');
 var ejs    = require('ejs');
@@ -66,9 +65,9 @@ module.exports = function (app, passport, mongoClient) {
     next();
   });
 
-  // bodyParser should be above methodOverride
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  // Body parsing should be configured before methodOverride
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
